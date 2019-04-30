3b98a07241add54c4150b09a5a2506b0d07db557
daehahn <daehyuk@hotmail.com>
2019-04-29 17:31:45 -0700

    Change install script
    For node installation and other packages.
    Still has open-zwave compile error (c11 specific)

--- automated_build.sh
+++ automated_build.sh
@@ -4,15 +4,14 @@
 sudo setcap cap_net_raw+eip $(eval readlink -f `which node`)
 sudo setcap cap_net_raw+eip $(eval readlink -f `which python3`)
 
-# Download and Build Gateway
-mkdir ~/mozilla-iot
-cd ~/mozilla-iot
-
 # Build and Install openzwave
-git clone https://github.com/OpenZWave/open-zwave.git
-cd open-zwave
-CFLAGS=-D_GLIBCXX_USE_CXX11_ABI=0 make && sudo CFLAGS=-D_GLIBCXX_USE_CXX11_ABI=0 make install
-sudo ldconfig
+#cd ~/mozilla-iot
+#if [ ! -d open-zwave ]; then
+#    git clone https://github.com/OpenZWave/open-zwave.git
+#fi    
+#cd open-zwave
+#CFLAGS=-D_GLIBCXX_USE_CXX11_ABI=0 make && sudo CFLAGS=-D_GLIBCXX_USE_CXX11_ABI=0 make install
+#sudo ldconfig
 
 # Add into ~/.profile 
 export LD_LIBRARY_PATH=/usr/local/lib/:$LD_LIBRARY_PATH
@@ -22,36 +21,48 @@ if [ "$ld_lib" = "" ] ; then
 fi
 
 # Intent Parser for the Mozilla WebThings Gateway
-git clone https://github.com/mozilla-iot/intent-parser.git
+cd ~/mozilla-iot
+if [ ! -d intent-parser ]; then
+    git clone https://github.com/mozilla-iot/intent-parser.git
+fi
 sudo python3 -m pip install git+https://github.com/mycroftai/adapt#egg=adapt-parser
 
 # Install Python Add-on Bindings (Optional)
-python2 -m pip install git+https://github.com/mozilla-iot/gateway-addon-python#egg=gateway_addon --user
-python3 -m pip install git+https://github.com/mozilla-iot/gateway-addon-python#egg=gateway_addon --user
+sudo python2 -m pip install git+https://github.com/mozilla-iot/gateway-addon-python#egg=gateway_addon --user
+sudo python3 -m pip install git+https://github.com/mozilla-iot/gateway-addon-python#egg=gateway_addon --user
 
 # Download lastest version of gateway
-git clone https://github.com/mozilla-iot/gateway.git
+cd ~/mozilla-iot
+if [ ! -d gateway ]; then
+    git clone https://github.com/mozilla-iot/gateway.git
+    # DB410c - Replace platform script and image for DB410c support
+    cp -r ./db410c/src/ ./gateway/src/
+    cp -r ./db410c/image/ ./gateway/image/
+fi
 
-# DB410c - Replace platform script and copy DB410c support
-cp db410c/src/* ~/mozilla-iot/gateway/src/
-cp db410c/image/*  ~/mozilla-iot/gateway/image
+# DB410c - Build nanomsg and sqlite3
+#cd ~/mozilla-iot
+#if [ -d ./gateway/node_modules/nanomsg ]; then 
+#    cd ~/mozilla-iot/gateway/node_modules/nanomsg
+#    make
+#fi
+#cd ~/mozilla-iot
+#if [ -d ./gateway/node_modules/sqlite3 ]; then
+#    cd ~/mozilla-iot/gateway/node_modules/sqlite3
+#    make
+#fi
 
-# DB410c - Build nanomsg and sqlite3 
-cd ~/mozilla-iot/gateway/node_modules/nanomsg
-make
-cd ~/mozilla-iot/gateway/node_modules/sqlite3
-make
-
-# Build node
-nvm install
-nvm use
-nvm alias default $(node -v)
-
+# Checking node verion
+cd ~/mozilla-iot/gateway
 node --version
 npm --version
 
 npm install
+npm audit fix
 npm start
+
+# Trace specific file 
+#npm install -g nodemon
 
 # Autostart - Please do this manually after testing
 #cp ~/mozilla-iot/gateway/image/prepare-base.sh ~/
--- automated_install.sh
+++ automated_install.sh
@@ -7,44 +7,55 @@ source ~/temp_swapfile.sh
 sudo apt-get update
 
 # Install pkg-config
-sudo apt install pkg-config
+sudo apt install -y pkg-config
 
 # Install Bluetooth and BT Low Energy support libraries (Linux only)
-sudo apt-get install libboost-python-dev libboost-thread-dev libbluetooth-dev libglib2.0-dev
+sudo apt-get install -y libboost-python-dev libboost-thread-dev libbluetooth-dev libglib2.0-dev
 
 # Install libusb and libudev (Linux only)
-sudo apt-get install libusb-1.0-0-dev libudev-dev
+sudo apt-get install -y libusb-1.0-0-dev libudev-dev
 
 # Install autoconf
-sudo apt-get install autoconf
+sudo apt-get install -y autoconf
 
 # Install libpng (Linux only)
-sudo apt-get install libpng-dev
+sudo apt-get install -y libpng-dev
 
 # Install git
-sudo apt-get install git
+sudo apt-get install -y git
 
 # Install gcc (needed to build open-zwave)
-sudo apt-get install build-essential
+sudo apt-get install -y build-essential
 
 # Install nanomsg
-sudo apt-get install libnanomsg4 libnanomsg-dev
+sudo apt-get install -y libnanomsg5 libnanomsg-dev
 
 # Install additional dependencies for Debian (where python points to python 2.7)
-sudo apt install -y python-pip python3-pip openjdk-8-jre #fire-fox
+sudo apt-get install -y python-pip python3-pip #openjdk-8-jre #fire-fox
 
 # Install Python cffi tool
 sudo apt-get install -y libffi-dev python-cffi
 
 # Install curl (needed to install nvm)
-sudo apt-get install curl
+sudo apt-get install -y curl
 
 # Install nvm (Recommended) - may be manually needed
 # curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-cd ~; wget https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh
-sudo chmod +x ~/install.sh
-source ~/install.sh
-. ~/.bashrc
+if [ ! -d ~/.nvm ]; then
+    cd ~; wget https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh
+    sudo chmod +x ~/install.sh
+    source ~/install.sh
+    source ~/.nvm/nvm.sh
+    source ~/.profile
+    source ~/.bashrc
+    nvm install 8.15
+    nvm use
+    nvm alias default $(node -v)
+fi
 
 # Let user know they need to run next script
-echo "Run next setup with 'bash automated_build.sh'"
+echo "=================================================="
+echo "* For loading NVM, please logout and login again *"
+echo "  and run next setup by 'bash automated_build.sh' "
+echo "=================================================="
+
